5e29b8cc7eea66d5462f48417ded99ecb16cd0fe
Danila <dyukov_danila@mail.ru>
2025-12-01 22:05:08 +0300

    feat: реализовал обработку кнопки + и обновил стили — завершение первого дня работы

--- src/App.js
+++ src/App.js
@@ -3,34 +3,46 @@ import './App.css';
 import { useState } from 'react';
 
 let coffeeList = [
-  { "name": "Classic Cappuccino", type: "cappuccino", image: "/images/Classic Cappuccino.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 530 },
-  { "name": "Iced Cappuccino", type: "cappuccino", image: "/images/Iced Cappuccino.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 750 },
-  { "name": "Cinnamon Cappuccino", type: "cappuccino", image: "/images/ Cinnamon Cappuccino..jpeg", size: "", milk: "", sugar: "", milkType: "", price: 530 },
-  { "name": "Double Shot Cappuccino", type: "cappuccino", image: "/images/Double Shot Cappuccino.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 750 },
+  { "name": "Classic Cappuccino", type: "cappuccino", image: "/images/Classic Cappuccino.jpeg", size: "",  extra:"", count: 0, milkType: "", price: 530 },
+  { "name": "Iced Cappuccino", type: "cappuccino", image: "/images/Iced Cappuccino.jpeg", size: "",  extra:"", count: 0, milkType: "", price: 750 },
+  { "name": "Cinnamon Cappuccino", type: "cappuccino", image: "/images/ Cinnamon Cappuccino..jpeg", size: "", extra:"", count: 0, milkType: "", price: 530 },
+  { "name": "Double Shot Cappuccino", type: "cappuccino", image: "/images/Double Shot Cappuccino.jpeg", size: "", extra:"", count: 0, milkType: "", price: 750 },
 
-  { "name": "Vanilla Latte", type: "latte", image: "/images/Vanilla Latte.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 750 },
-  { "name": "Caramel Latte", type: "latte", image: "/images/ Caramel Latte.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 790 },
-  { "name": "Iced Latte", type: "latte", image: "/images/iced Latte.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 750 },
-  { "name": "Matcha Latte", type: "latte", image: "/images/Matcha Latte.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 790 },
+  { "name": "Vanilla Latte", type: "latte", image: "/images/Vanilla Latte.jpeg", size: "", extra:"", count: 0, milkType: "", price: 750 },
+  { "name": "Caramel Latte", type: "latte", image: "/images/ Caramel Latte.jpeg", size: "", extra:"", count: 0, milkType: "", price: 790 },
+  { "name": "Iced Latte", type: "latte", image: "/images/iced Latte.jpeg", size: "",  extra:"", count: 0, milkType: "", price: 750 },
+  { "name": "Matcha Latte", type: "latte", image: "/images/Matcha Latte.jpeg", size: "", extra:"", count: 0, milkType: "", price: 790 },
 
-  { "name": "Iced Americano", type: "americano", image: "/images/Iced Americano.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 570 },
-  { "name": "Americano with Lemon", type: "americano", image: "/images/ Americano with Lemon.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 570 },
+  { "name": "Iced Americano", type: "americano", image: "/images/Iced Americano.jpeg", size: "",  extra:"", count: 0, milkType: "", price: 570 },
+  { "name": "Americano with Lemon", type: "americano", image: "/images/ Americano with Lemon.jpeg", size: "", extra:"", count: 0, milkType: "", price: 570 },
 
-  { "name": "Flat White", type: "flatwhite", image: "/images/ Flat White.jpeg", size: "", milk: "", sugar: "", milkType: "", price: 750 }
+  { "name": "Flat White", type: "flatwhite", image: "/images/ Flat White.jpeg", size: "", milk: "", count: 0, price: 750 }
 ];
 
 
-const ShowCoffeeList = ({filteredCoffee}) => {
+const ShowCoffeeList = ({filteredCoffee, AddCoffeeInBascket}) => {
   return coffeeList.filter(t =>filteredCoffee === "All" || t.type === filteredCoffee.trim()).map(e => (
     <div className="coffeeCard">
         <img src={e.image} className="coffeeImage"></img>
         <h2 className="coffeeName">{e.name}</h2>
-        <h3 className="coffeePrice">{e.price} RUB</h3>
+        <div className='footerCoffeeCard'>
+          <button className={`removeToBasketButton ${e.count>0 ? "" : "invisible"}`}>-</button>
+          <h3 className="coffeePrice">{e.price} RUB</h3>
+          <button className="addToBasketButton" onClick={() => AddCoffeeInBascket(e)}>+</button>
+        </div>
+        <h2 className={`coffeeCountInBasket ${e.count>0 ? "" : "invisible"}`}>In Basket: {e.count}</h2>
       </div>
   ));
 }
 function App() {
   const [filteredCoffee, setFilteredCoffeeList] = useState("All");
+  const [whichCoffeesInBasket, setWhichCoffeesInBasket] = useState({});
+
+  const AddCoffeeInBascket = (Coffee) => {
+    Coffee.count += 1;
+    setWhichCoffeesInBasket({...whichCoffeesInBasket, Coffee});
+  };
+
   return (
   <div>
     <div className="mainBox">
@@ -62,7 +74,7 @@ function App() {
           </div>
         </div>
         <div className="mainBoxCoffee">
-          {<ShowCoffeeList filteredCoffee={filteredCoffee}/>}
+          {<ShowCoffeeList filteredCoffee={filteredCoffee} AddCoffeeInBascket={AddCoffeeInBascket}/>}
         </div>
       </div>
     </div>

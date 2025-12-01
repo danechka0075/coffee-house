d22dac9dc7bd60ed2ed8b74d0065c39648e555b9
Danila <dyukov_danila@mail.ru>
2025-12-01 15:51:17 +0300

    feat: добавил начальную верстку главной страницы

--- src/App.js
+++ src/App.js
@@ -6,20 +6,39 @@ import './App.css';
 function App() {
   return (
   <div>
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        Learn React
-      </a>
-    </header>           
+    <div className="mainBox">
+      <aside className="leftBox">
+        <div className="scrollBox">
+          <div className="categoryCoffees">
+            <h2 className="textCategoryCoffee">All</h2>
+          </div>
+          <div className="categoryCoffees">
+            <h2 className="textCategoryCoffee">Cappuccino</h2>
+          </div>
+          <div className="categoryCoffees">
+            <h2 className="textCategoryCoffee">Latte</h2>
+          </div>
+          <div className="categoryCoffees">
+            <h2 className="textCategoryCoffee">Espresso</h2>
+          </div>
+          <div className="categoryCoffees">
+            <h2 className="textCategoryCoffee">Americano</h2>
+          </div>
+        </div>
+      </aside>
+      <div className="rightBox">
+        <div className="headerBox">
+          <input className="searchInput" placeholder="Browse your favourite coffee here.."/>
+          <div className="basketBox">
+            <img className="basketImage" src="https://cdn-icons-png.flaticon.com/512/1170/1170678.png" alt="basket"/>
+            <h2 className="basketCount">0</h2>
+          </div>
+        </div>
+        <div className="mainBox">
+        
+        </div>
+      </div>
+    </div>
   </div>
   );
 }
